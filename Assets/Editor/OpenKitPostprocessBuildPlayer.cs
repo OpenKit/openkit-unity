b0f86aef3ef2a5eb56dc33cf259db99cdc3b4486
Suneet Shah <suneets@gmail.com>
2013-03-25 16:26:36 -0700

    update Android jars to latest, cleaned up the android manifest, moved the declaration of Facebook App ID to c# script

--- Assets/Editor/OpenKitPostprocessBuildPlayer.cs
+++ Assets/Editor/OpenKitPostprocessBuildPlayer.cs
@@ -5,13 +5,19 @@ using UnityEditor.Callbacks;
 using System.Diagnostics;
 
 public class OpenKitPostprocessBuildPlayer : MonoBehaviour {
+	
+	// You must set your Facebook application ID below.
+	// For more info, see http://openkit.io/docs/unity/integration.html
+	// The App ID listed below is a sample
+	public static string FacebookAppID = "155855667911346";
+	
 	[PostProcessBuild]
 	public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
 #if UNITY_IOS
 		Process proc = new Process();
 		proc.EnableRaisingEvents=false; 
 		proc.StartInfo.FileName = Application.dataPath + "/Plugins/OpenKit/PostbuildScripts/PostBuildOpenKitIOSScript";
-		proc.StartInfo.Arguments = "'" + pathToBuiltProject + "'";
+		proc.StartInfo.Arguments = "'" + pathToBuiltProject + "' '" + FacebookAppID + "'";
 		proc.Start();
 		proc.WaitForExit();
 		UnityEngine.Debug.Log("OpenKit iOS build log file: " + System.IO.Directory.GetCurrentDirectory() + "/OpenKitIOSBuildLogFile.txt");

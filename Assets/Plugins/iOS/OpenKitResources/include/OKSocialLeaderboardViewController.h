26f785b67fe04fd217e30405d4113fe72e469e04
Lou Zell <lzell11@gmail.com>
2013-06-26 02:16:32 -0700

    Updated native iOS plugin up to sha: 7a62bde

--- Assets/Plugins/iOS/OpenKitResources/include/OKSocialLeaderboardViewController.h
+++ Assets/Plugins/iOS/OpenKitResources/include/OKSocialLeaderboardViewController.h
@@ -14,11 +14,15 @@
 
 @property (nonatomic, strong) OKLeaderboard *leaderboard;
 @property (nonatomic, strong) IBOutlet UITableView *_tableView;
-@property (nonatomic, strong) IBOutlet UIButton *moreBtn;
 @property (nonatomic, strong) IBOutlet UIActivityIndicatorView *spinner;
+
+@property (nonatomic, strong) IBOutlet UIView *containerViewForLoadMoreButton;
+@property (nonatomic, strong) IBOutlet UIButton *loadMoreScoresButton;
 
 @property (nonatomic, strong) NSMutableArray *globalScores, *socialScores;
 
 - (id)initWithLeaderboard:(OKLeaderboard *)aLeaderboard;
+
+-(IBAction)loadMoreScoresPressed:(id)sender;
 
 @end

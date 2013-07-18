abe1c001d9ca94be9aa8e44f92f8ad39ed8aa6cc
Lou Zell <lzell11@gmail.com>
2013-07-17 17:36:32 -0700

    Using hardlinks to bring Editor and Plugins directories:
    Both Unity3Sample and Unity4Sample use the same Editor and
    Plugins directories.  Unity doesn't appear to work well with
    symlinks.  Copying files around was too much to keep track of,
    and a git submodule for both Plugins and Editor directories
    seemed like overkill.
    
    The following directories are now ignored:
    Unity3Sample/Assets/Plugins
    Unity3Sample/Assets/Editor
    Unity4Sample/Assets/Plugins
    Unity4Sample/Assets/Editor
    
    A hardlink should be created between OKPlugins and:
    Unity3Sample/Assets/Plugins
    Unity4Sample/Assets/Plugins
    
    A hardlink should be created between OKEditor and:
    Unity3Sample/Assets/Editor
    Unity4Sample/Assets/Editor

---
+++ OKEditor/OpenKitPostprocessBuildPlayer.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Diagnostics;
+
+public class OpenKitPostprocessBuildPlayer : MonoBehaviour {
+
+	// You must set your Facebook application ID below.
+	// For more info, see http://openkit.io/docs/unity/integration.html
+	// The App ID listed below is a sample
+	public static string FacebookAppID = "450333868362300";
+
+	[PostProcessBuild]
+	public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
+#if UNITY_IOS
+		Process proc = new Process();
+		proc.EnableRaisingEvents=false;
+		proc.StartInfo.FileName = Application.dataPath + "/Plugins/OpenKit/PostbuildScripts/PostBuildOpenKitIOSScript";
+		proc.StartInfo.Arguments = "'" + pathToBuiltProject + "' '" + FacebookAppID + "'";
+		proc.Start();
+		proc.WaitForExit();
+		UnityEngine.Debug.Log("OpenKit iOS build log file: " + System.IO.Directory.GetCurrentDirectory() + "/OpenKitIOSBuildLogFile.txt");
+		UnityEngine.Debug.Log("OpenKit path to built project: " + pathToBuiltProject);
+#endif
+    }
+}
--- README.md
+++
@@ -1,7 +0,0 @@
-openkit-unity
-=============
-
-Please see: http://openkit.io/docs/unity/
-
-For instructions on building android unity plugin, see openkit-android repo at: 
-openkit-android/UnityAndroid/README.md

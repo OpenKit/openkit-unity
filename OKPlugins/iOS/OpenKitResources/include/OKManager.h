31cbb554e91a87c4d7e102107cb61e7f34008f66
Suneet Shah <suneets@gmail.com>
2013-09-10 19:53:08 -0700

    updated to latest debug versions of iOS and Android SDKs from development branch

--- OKPlugins/iOS/OpenKitResources/include/OKManager.h
+++ OKPlugins/iOS/OpenKitResources/include/OKManager.h
@@ -12,6 +12,8 @@
 @class OKUser;
 @interface OKManager : NSObject
 
++ (void)configureWithAppKey:(NSString *)appKey secretKey:(NSString *)secretKey;
++ (void)configureWithAppKey:(NSString *)appKey secretKey:(NSString *)secretKey endpoint:(NSString *)endpoint;
 + (id)sharedManager;
 - (void)saveCurrentUser:(OKUser *)aCurrentUser;
 - (void)logoutCurrentUser;
@@ -36,11 +38,9 @@
 //    manager.appKey     = "foo";
 //    manager.secretKey  = "bar";
 //
-+ (void)setAppKey:(NSString *)appKey;
+
 + (NSString *)appKey;
-+ (void)setEndpoint:(NSString *)endpoint;
 + (NSString *)endpoint;
-+ (void)setSecretKey:(NSString *)secretKey;
 + (NSString *)secretKey;
 
 + (BOOL)handleOpenURL:(NSURL*)url;

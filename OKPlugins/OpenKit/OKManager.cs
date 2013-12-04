13604d1fa0db65b36eeffef23e21de2da427a42f
Suneet Shah <suneets@gmail.com>
2013-12-03 17:45:11 -0800

    set SDK version to 1.0.4

--- OKPlugins/OpenKit/OKManager.cs
+++ OKPlugins/OpenKit/OKManager.cs
@@ -8,7 +8,7 @@ namespace OpenKit
 {
 	public class OKManager
 	{
-		public const string OPENKIT_SDK_VERSION = "1.0.3";
+		public const string OPENKIT_SDK_VERSION = "1.0.4";
 		private const string DEFAULT_ENDPOINT = "http://api.openkit.io";
 
 		private string _LeaderboardListTag = null;

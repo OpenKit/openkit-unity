64662b0a0daa37496536996c048837677862d711
Suneet Shah <suneets@gmail.com>
2014-01-08 13:47:38 -0800

    added a menu item and script to export the Unity package

---
+++ OpenKitUnityPlugin/Assets/OpenKitInternal/OpenKitBuildMenu.cs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using OpenKit;
+
+public class OpenKitBuildMenu : EditorWindow {
+
+	[MenuItem("OpenKit/ExportPackage")]
+	public static void ShowWindow()
+	{
+		string[] OpenKitAssetPaths = {
+			"Assets/Editor/OpenKitPostprocessBuildPlayer.cs",
+			"Assets/Editor/OpenKitSettingsWindow.cs",
+			"Assets/Examples/OKDemoScene.cs",
+			"Assets/Examples/OKDemoScene.unity",
+			"Assets/Plugins/Android",
+			"Assets/Plugins/iOS",
+			"Assets/Plugins/OpenKit",
+			"Assets/Plugins/RestSharp.dll",
+			"Assets/Prefabs/OpenKitPrefab.prefab"
+		};
+
+		string SDKVersion = OKManager.OPENKIT_SDK_VERSION;
+
+		string PackageName = "OpenKitUnityPlugin." + SDKVersion + ".unitypackage";
+
+		AssetDatabase.ExportPackage(OpenKitAssetPaths,PackageName,ExportPackageOptions.Recurse);
+
+		Debug.Log ("Exported OpenKit Package");
+	}
+
+}
